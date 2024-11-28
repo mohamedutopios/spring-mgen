dba67ef732ccbfce3831be73e3308445d05ecaaf
mohamed <mohamed@utopios.net>
2024-11-28 14:22:31 +0100

    :tada: first commit

--- thymeleaf/src/main/java/org/example/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/org/example/thymeleaf/controller/UserController.java
@@ -1,10 +1,20 @@
 package org.example.thymeleaf.controller;
 
 
+import org.example.thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+
+    @GetMapping
+    public String user(Model model) {
+        model.addAttribute("user", new User("Bala",56,"active"));
+        return "user";
+    }
 
 
 
--- thymeleaf/src/main/java/org/example/thymeleaf/model/User.java
+++ thymeleaf/src/main/java/org/example/thymeleaf/model/User.java
@@ -16,5 +16,4 @@ public class User {
     private Integer age;
     private String status;
 
-
 }
