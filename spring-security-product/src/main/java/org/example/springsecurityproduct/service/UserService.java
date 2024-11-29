1a1249318fec87c0cc56ec028272604aea814822
mohamed <mohamed@utopios.net>
2024-11-29 10:15:15 +0100

    add config

---
+++ spring-security-product/src/main/java/org/example/springsecurityproduct/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.springsecurityproduct.service;
+
+import org.example.springsecurityproduct.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + email));
+    }
+}
